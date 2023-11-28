f018f7669b4f56e4c1b15ba25f1110a25bef9be2
MarcosGui <mgui@li-jiang.com>
2023-11-28 20:29:53 +0800

    feat: remove unused files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'ns-app',
-  templateUrl: './app.component.html',
-  styleUrls:['./app.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {}
